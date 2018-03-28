7b64c0231afc6cefaba72c703e84720247e4dadf
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-03-28 10:49:10 -0400

    arrow-functions-part-1 -exercise 3

--- exercises/arrow-functions-part-1.js
+++ exercises/arrow-functions-part-1.js
@@ -1,6 +1,6 @@
 const inputs = process.argv.slice(2);
 
 const result = inputs.map(str => str.charAt(0))
-                   .reduce((soFar, str) => soFar + s);
+                   .reduce((soFar, str) => soFar + str);
 
 console.log(`[${inputs}] becomes "${result}"`);

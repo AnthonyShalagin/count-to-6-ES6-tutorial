74dc14afd7d5fe60dea17a65307dc81c48cbad10
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-03-28 14:00:05 -0400

    spread exercise 6

---
+++ exercises/spread.js
@@ -0,0 +1,12 @@
+/*
+You'll be given a variable number of arguments (process.argv[2] onward) to your program, all of which will be numbers. Use the ES6 spread operator to find the minimum value of these numbers and log it to the console.
+
+And let's log the list of numbers themselves, to make it clearer. So the output should be, for example:
+
+    The minimum of [18,5,7,24] is 5
+*/
+
+const numbers = process.argv.slice(2);
+const min = Math.min(...numbers);
+
+console.log(`The minimum of [${numbers}] is ${min}`);
